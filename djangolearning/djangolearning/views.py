d8e0b607953411d85b4202434374f61b19b50039
sparsh15 <sparsh.1518@gmail.com>
2025-06-24 16:27:06 +0530

    templates and dir

--- djangolearning/djangolearning/views.py
+++ djangolearning/djangolearning/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hello World. Welcome to home page")
+    #return HttpResponse("Hello World. Welcome to home page")
+    return render(request, 'website/index.html')
 
 def about(request):
     return HttpResponse("Hello World. Welcome to about page")
