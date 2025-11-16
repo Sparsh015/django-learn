6ee8589497c3eae74485d2889d9151ec441f804f
sparsh15 <sparsh.1518@gmail.com>
2025-11-16 16:33:12 +0530

    new model and migrations

--- djangolearning/DJapp/models.py
+++ djangolearning/DJapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Varity(models.Model):
+
+    ANIME_TYPE_CHOICE = [
+        ('KB', 'Kuroku Basketball'),
+        ('ON', 'One Piece'),
+        ('DR', 'Dragon Ball Z'),
+        ('NT', 'Naruto'),
+        ('AT', 'Attack on Titan'),
+        ('DS', 'Demon Slayer'),
+    ]
+
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to = 'imgs/')
+    date_added = models.DateTimeField(default = timezone.now)
+    type = models.CharField(max_length=2, choices = ANIME_TYPE_CHOICE)
+    
