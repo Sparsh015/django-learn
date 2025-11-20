5d6042587640c5c0758fdf38cc34833761cbe9e3
sparsh15 <sparsh.1518@gmail.com>
2025-11-21 02:28:28 +0530

    another field added in model

--- djangolearning/DJapp/models.py
+++ djangolearning/DJapp/models.py
@@ -18,6 +18,8 @@ class Varity(models.Model):
     image = models.ImageField(upload_to = 'imgs/')
     date_added = models.DateTimeField(default = timezone.now)
     type = models.CharField(max_length=2, choices = ANIME_TYPE_CHOICE)
+    description = models.TextField(default='')
+    
 
     def __str__(self):
         return self.name
