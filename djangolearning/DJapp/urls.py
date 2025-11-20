e5bff4ed830f87e2b069e18e6bc69d1da8876d8e
sparsh15 <sparsh.1518@gmail.com>
2025-11-21 02:52:46 +0530

    individual view when click on button

--- djangolearning/DJapp/urls.py
+++ djangolearning/DJapp/urls.py
@@ -6,5 +6,6 @@ from . import views
 #localhost:8000/DJapp
 urlpatterns = [  
     path('', views.all_dj, name = 'all_dj'),
-
+    path('<int:anime_id>/', views.anime_detail, name = 'anime_detail'),
+    
 ] 
--- djangolearning/DJapp/views.py
+++ djangolearning/DJapp/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from .models import Varity
+from django.shortcuts import get_object_or_404
+
 # Create your views here.
 def all_dj(request):
     animes = Varity.objects.all()
     return render(request, 'DJapp/all_dj.html', {'animes': animes})
 
-
-
+def anime_detail(request, anime_id):
+    anime = get_object_or_404(Varity, pk= anime_id)
+    return render(request, 'DJapp/anime_detail.html', {'anime': anime})
