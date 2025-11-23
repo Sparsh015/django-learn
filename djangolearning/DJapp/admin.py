41d1c9a4a41e1e15acde0e408c242388e61018c6
sparsh15 <sparsh.1518@gmail.com>
2025-11-23 15:40:47 +0530

    more anime models in admin

--- djangolearning/DJapp/admin.py
+++ djangolearning/DJapp/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
-from .models import Varity
+from .models import Varity, AnimeReview, Store, AnimeCertificate
 
 # Register your models here.
+class AnimeReviewInline(admin.TabularInline):
+    model = AnimeReview
+    extra = 2
+
+class VarityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'date_added')
+    inlines = [AnimeReviewInline]
+
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location')
+    filter_horizontal = ('anime_varieties',)
+
+class AnimeCertificateAdmin(admin.ModelAdmin):
+    list_display = ('anime', 'certificate_number')
+
+
 admin.site.register(Varity)
--- djangolearning/DJapp/models.py
+++ djangolearning/DJapp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import user
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -21,12 +21,43 @@ class Varity(models.Model):
     type = models.CharField(max_length=2, choices = ANIME_TYPE_CHOICE)
     description = models.TextField(default='')
     
+    #names changes in the database and dont display as "objects"
+    def __str__(self):
+        return self.name
+
+
+# one to many relationship
+
+class AnimeReview(models.Model):
+    anime = models.ForeignKey(Varity, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
+    date_added = models.DateTimeField(default=timezone.now)
+
+    #names changes in the database and dont display as "objects"
+    def __str__(self):
+        return f'{self.user.username} review for {self.anime.name}'
+    
+
+# many to many relationship
+
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=200)
+    anime_varieties = models.ManyToManyField(Varity, related_name='stores')
 
     def __str__(self):
         return self.name
     
-#one to many relationship
 
-class AnimeReview(models.Model):
-    anime = models.ForeignKey(Varity, on_delete=models.CASCADE, related_name='reviews')
-    user = models.ForeignKey(user, on_delete=models.CASCADE)
+# one to one relationship
+
+class AnimeCertificate(models.Model):
+    anime = models.OneToOneField(Varity, on_delete=models.CASCADE, related_name='certificate')
+    certificate_number = models.CharField(max_length=50)
+    issue_date = models.DateTimeField(default=timezone.now)
+    valid_unitl = models.DateTimeField()
+
+    def __str__(self):
+        return f'Certificate for {self.anime.name}'
